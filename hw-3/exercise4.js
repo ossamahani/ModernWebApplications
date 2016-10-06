73d01d88e291fc382ee66cbf38bda8cfebac16e3
ossamahani <ossama.hani@gmail.com>
2016-10-06 01:11:46 -0500

    Stream and Files in Node.js

---
+++ hw-3/exercise4.js
@@ -0,0 +1,42 @@
+
+var fs = require("fs");
+const myPath = "./cs572.txt";
+
+//1
+var myData = fs.readFileSync(myPath,"utf-8");
+var buffer = new Buffer(myData, "utf-8");
+console.log(myData);
+console.log(buffer.length);
+
+//2
+var readableStream = fs.createReadStream(myPath, {encoding:'utf-8', hightWaterMark:1});
+readableStream.on("data", function(chunk){
+    for(var i=10; i<14; i++)
+    {
+        console.log("MyChuck " + chunk[i]);
+    }
+}).on("end", function () {
+    console.log("finished chunk")
+});
+
+//3
+var overrideStr = "ABCDEFGHIJLMKOPQRSTUVWXYZ1234567890abcdefghijklmnopqrstuvwxyz";
+fs.writeFile(myPath, overrideStr, function(err)
+                                                {
+                                                   if (err) throw err;
+                                                   console.log("done");
+                                                }
+                                            );
+//4
+fs.appendFile(myPath, "abc");
+myData = fs.readFileSync(myPath, 'utf-8')
+console.log(myData);
+
+
+//5
+
+
+buffer[10] = "7";
+fs.writeFile(myPath, buffer);
+myData = fs.readFileSync(myPath, 'utf-8')
+console.log(myData);
