3f9221e38e42958aed01a8a1ad1078244fc9d5ff
ossamahani <ossama.hani@gmail.com>
2016-10-06 22:46:15 -0500

    Add Get method

--- hw-4/exercise2.js
+++ hw-4/exercise2.js
@@ -3,7 +3,11 @@ const fs = require("fs");
 const queryString = require('querystring');
 
 function onRequest(request, response){
-    if(request.url === "/upload")
+    if(request.url === "/")
+    {
+        fs.createReadStream(__dirname+"/index.html").pipe(response);
+    }
+    else if(request.url === "/upload")
     {
         var postData = "";
         request.setEncoding("utf8");
@@ -15,8 +19,11 @@ function onRequest(request, response){
                 console.log("Data Recieved " + myTextAreaValue);
                 fs.writeFile("./userInput.txt", myTextAreaValue, function(err){ if(err) throw err});
             });
-    }    
-    response.end("Your data is submitted Successfully");
+        response.end("Your data is submitted Successfully");    
+    }else{
+        response.writeHead(404);
+        response.end("Error page, the requested path is not exist");
+    }
 }
 
 http.createServer(onRequest).listen(8888);
