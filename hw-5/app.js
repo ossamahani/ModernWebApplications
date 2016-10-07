399b2f892f6c7b13078df27bec313d64354d4ae8
Ossama Hani <ossama.hani@gmail.com>
2016-10-07 15:12:51 -0500

    install ejs lib

--- hw-5/app.js
+++ hw-5/app.js
@@ -1,3 +1,13 @@
 var app = require('express')();
 
+app.set('view engine', 'ejs');
+
+
+
+app.get("/inventors", function(request, response){
+
+    
+
+    response.render(__dirname+'/index.ejs', {title:'MWA'});
+});
 app.listen(8888, ()=>console.log(`listening to 8888`))
