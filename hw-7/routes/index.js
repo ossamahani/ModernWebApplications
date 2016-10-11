74fc4d26370b2b4cdd7df641c06ce10e7be9bf4d
ossamahani <ossama.hani@gmail.com>
2016-10-10 20:33:45 -0500

    Decrypt the read message from MongoDB

--- hw-7/routes/index.js
+++ hw-7/routes/index.js
@@ -2,12 +2,27 @@ var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 var url = 'mongodb://localhost:27017/dbtest';
+
+
+var crypto = require('crypto'),
+    algorithm = 'aes256',
+    password = 'asaadsaad';
+
+function decrypt(text){
+  var decipher = crypto.createDecipher(algorithm,password)
+  var dec = decipher.update(text,'hex','utf8')
+  dec += decipher.final('utf8');
+  return dec;
+}    
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
       MongoClient.connect(url, function(err, db) {
             db.collection('lab1').findOne({},function(err, doc){
                  if(err) throw err;
-                 res.render('index', { title: 'My Encrpted Message', message:doc.message});
+                 res.render('index', { title: 'My Decrypted Message', message:decrypt(doc.message)});
       });
     });
 });
