c521fa7f63781d260127c6674feaa09cadca13af
ossamahani <ossama.hani@gmail.com>
2016-10-13 16:49:51 -0500

    Start hw-9

---
+++ hw-9/routes/index.js
@@ -0,0 +1,42 @@
+var express = require('express');
+var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+var url = 'mongodb://localhost:27017/dbtest';
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Add Location', message:''});
+});
+
+
+router.get('/search', function(req, res, next) {
+  res.render('search', { title: 'Search Location'});
+});
+
+
+router.get('/search', function(req, res, next) {
+  MongoClient.connect(url, function(err, db) {
+            db.collection('lab9').insert({name:req.body.name, category:req.body.category,location:[long, lat]},function(err){
+                 if(err) throw err;
+                 db.close();
+        });
+   });
+});
+
+
+router.post('/save', function(req, res, next) {
+   MongoClient.connect(url, function(err, db) {
+            var lat = parseFloat(req.body.lat);
+            var long = parseFloat(req.body.long);
+            db.collection('lab9').insert({name:req.body.name, category:req.body.category,location:[long, lat]},function(err){
+                 if(err) throw err;
+                 req.body.name = '';
+                 req.body.lat = '';
+                 req.body.long = '';
+                 res.render('index', { title: 'Add Location', message : "Location Added Successfully"});
+                 db.close();
+        });
+   });
+});
+
+module.exports = router;
