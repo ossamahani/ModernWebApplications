492507ea93c3db8ef76bf6fff07590432735f651
ossamahani <ossama.hani@gmail.com>
2016-10-18 23:14:22 -0500

    Add UI Master/Details to the Prodcut List

--- angular-quickstart/dist/app.component.js
+++ angular-quickstart/dist/app.component.js
@@ -9,13 +9,37 @@ var __metadata = (this && this.__metadata) || function (k, v) {
     if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
 };
 var core_1 = require('@angular/core');
+var Product = (function () {
+    function Product() {
+    }
+    return Product;
+}());
+exports.Product = Product;
+var PRODUCTS = [
+    { id: 11, name: 'Iphone' },
+    { id: 12, name: 'Samsung Tablet' },
+    { id: 13, name: 'Samsung Note3' },
+    { id: 14, name: 'Lenovo' },
+    { id: 15, name: 'HP' },
+    { id: 16, name: 'DELL Latitude' },
+    { id: 17, name: 'Toshiba' },
+    { id: 18, name: 'ACER' },
+    { id: 19, name: 'Mac Notebook' },
+    { id: 20, name: 'Kindle' }
+];
 var AppComponent = (function () {
     function AppComponent() {
+        this.title = 'Tour of Products';
+        this.products = PRODUCTS;
     }
+    AppComponent.prototype.onSelect = function (product) {
+        this.selectedProduct = product;
+    };
     AppComponent = __decorate([
         core_1.Component({
             selector: 'my-app',
-            template: '<h1>My First Angular App</h1>'
+            template: "\n    <h2>My Products</h2>\n    <ul class=\"products\">\n      <li *ngFor=\"let product of products\" [class.selected]=\"product === selectedProduct\" (click)=\"onSelect(product)\">\n           <span class=\"badge\">{{product.id}}</span> {{product.name}}\n      </li>\n    </ul>\n    <div *ngIf=\"selectedProduct\">\n      <h2>{{selectedProduct.name}} details!</h2>\n      <div><label>id: </label>{{selectedProduct.id}}</div>\n      <div>\n        <label>name: </label>\n        <input [(ngModel)]=\"selectedProduct.name\" placeholder=\"name\"/>\n      </div> \n    </div>   \n  ",
+            styles: ["\n  .selected {\n    background-color: #CFD8DC !important;\n    color: white;\n  }\n  .products {\n    margin: 0 0 2em 0;\n    list-style-type: none;\n    padding: 0;\n    width: 15em;\n  }\n  .products li {\n    cursor: pointer;\n    position: relative;\n    left: 0;\n    background-color: #EEE;\n    margin: .5em;\n    padding: .3em 0;\n    height: 1.6em;\n    border-radius: 4px;\n  }\n  .products li.selected:hover {\n    background-color: #BBD8DC !important;\n    color: white;\n  }\n  .products li:hover {\n    color: #607D8B;\n    background-color: #DDD;\n    left: .1em;\n  }\n  .products .text {\n    position: relative;\n    top: -3px;\n  }\n  .products .badge {\n    display: inline-block;\n    font-size: small;\n    color: white;\n    padding: 0.8em 0.7em 0 0.7em;\n    background-color: #607D8B;\n    line-height: 1em;\n    position: relative;\n    left: -1px;\n    top: -4px;\n    height: 1.8em;\n    margin-right: .8em;\n    border-radius: 4px 0 0 4px;\n  }\n"]
         }), 
         __metadata('design:paramtypes', [])
     ], AppComponent);
